7974d7d784eb9ab521b091541231ca42a7cdabd4
SaiSreenivas_nehask_Datascitist <sreenivassaibks@gmail.com>
2025-02-03 01:06:37 +0530

    added to all files for upto get_datafiles

---
+++ get_data.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import json
+
+from dotenv import load_dotenv
+
+import certifi
+import pandas as pd
+import numpy as np
+import pymongo
+
+#MLOPS_adv\networksecurity\exception\__init__.py
+
+from networksecurity.exception import NetworkSecurityException
+from networksecurity.logger import logging
+
+
+class NetworkDataExtract():
+    def __iniit__(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+    def csv_to_json_convertor(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+    def pushing_data_to_mongodb(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+
+    if __name__ =='__main__':
+        pass
